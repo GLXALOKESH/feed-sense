672db7f5d64def6533aa77d36400c6386214a511
GLXALOKESH <alokeshmaitra9@gmail.com>
2025-08-23 12:12:06 +0530

    initial frontend setup done

---
+++ Client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+function App() {
+
+  return (
+    <>
+     <RouterProvider router={router} />,
+    </>
+  )
+}
+
+export default App
